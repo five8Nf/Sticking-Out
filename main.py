301ae25219af837fa5b9cff133ea801f1f01e2aa
five8Nf <ashtonlimex@gmail.com>
2026-06-10 09:15:53 +0800

    Added game name to top

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from creatures import *
 
 pygame.init()
 
+pygame.display.set_caption("Sticking Out")
 WINDOW_SIZE = (400, 400)
 screen = pygame.display.set_mode(WINDOW_SIZE)
 clock = pygame.time.Clock()
