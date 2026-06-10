1fa773539860901c36779d8db9e9b44046900f02
five8Nf <ashtonlimex@gmail.com>
2026-06-10 20:58:16 +0800

    grav

--- creatures.py
+++ creatures.py
@@ -10,6 +10,8 @@ class Tile():
     def __init__(self, img, x, y):
         self.x = x
         self.y = y
+        self.speed_y
+        self.ground_collision = False
         self.img = pygame.image.load(img).convert_alpha()
         self.img = pygame.transform.scale(self.img, (100, 100))
     
@@ -19,7 +21,7 @@ class Tile():
 class Player:
     def __init__(self, img, hp):
         self.x = 320
-        self.y = 200
+        self.y = 350
         self.hp = hp
         self.frame = 1
         self.img = pygame.image.load(img).convert_alpha()
@@ -44,6 +46,8 @@ class Enemy:
     def __init__(self, img, hp):
         self.x = randint(0, 640)
         self.y = randint(0, 400)
+        self.speed_y = 0
+        self.ground_collision = False
         self.hp = hp
         self.img = pygame.image.load(img).convert_alpha()
         self.img = pygame.transform.scale(self.img, (100, 100))
--- main.py
+++ main.py
@@ -14,24 +14,26 @@ player = Player("Assets/PlayerL1.png", 100)
 spawn_enemy(5, "Assets/BottleL1.png")
 
 speed = 5
+gravity = 0.5
+
+def jump(thing):
+    thing.speed_y += gravity
+    thing.y += thing.speed_y
+
 def movement(keys):
     player.movement(keys)
     for enemy in enemies:
-        if keys[pygame.K_w]:
-            enemy.y += speed
+        if keys[pygame.K_w] and enemy.ground_collision:
+            enemy.speed_y = -enemy.jump_height
         if keys[pygame.K_a]:
             enemy.x += speed
-        if keys[pygame.K_s]:
-            enemy.y -= speed
         if keys[pygame.K_d]:
             enemy.x -= speed
     for tile in tiles:
-        if keys[pygame.K_w]:
-            tile.y += speed
+        if keys[pygame.K_w] and tile.ground_collision:
+            tile.speed_y = 0 - tile.jump_height
         if keys[pygame.K_a]:
             tile.x += speed
-        if keys[pygame.K_s]:
-            tile.y -= speed
         if keys[pygame.K_d]:
             tile.x -= speed
 
@@ -43,28 +45,35 @@ walls = {"Top" : [],
          }
 
 def level_rect(amt_x, amt_y, x, y):
-    
+    walls["Top"].append(pygame.Rect(x, y, int(100*amt_x), 1))
     for _ in range(amt_x):
         tiles.append(Tile("Assets/Tile_down.png", x, y))
-        x += 100
+        x -= 100
 
 
 running = True
 state = "playing"
-level_rect(100, 1, 100, 100)
+level_rect(100, 1, 320, 450)
 while running:
     dt = clock.tick(60) / 1000
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     if state == "playing":
-        screen.fill((30, 80, 30))
+        screen.fill((0, 0, 0))
         keys = pygame.key.get_pressed()
         movement(keys)
         player.draw(screen)
         for enemy in enemies:
+            hitbox = pygame.Rect(enemy.x, enemy.y, 50, 100)
+            for base in walls["Top"]:
+                enemy.ground_collision = hitbox.colliderect(base)
+            jump(enemy)
             enemy.draw(screen)
         for tile in tiles:
+            jump(tile)
             tile.draw(screen)
+        # for _, value in walls.items():
+            
     pygame.display.update()
 pygame.quit()
