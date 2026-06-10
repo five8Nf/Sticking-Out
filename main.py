3acab458eecd28e28e1162cfcbe98b58a13eff52
five8Nf <ashtonlimex@gmail.com>
2026-06-10 10:55:48 +0800

    main

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ WINDOW_SIZE = (400, 400)
 screen = pygame.display.set_mode(WINDOW_SIZE)
 clock = pygame.time.Clock()
 
-player = Player("Main Glue Left/Mainglue.pg1.png", 100)
-spawn_enemy(5, "Bottle Right/Bottle.pg1.png")
+player = Player("Assets/PlayerL1.png", 100)
+# spawn_enemy(5, "Assets/BottleL1.png")
 
 running = True
 state = "playing"
@@ -29,34 +29,34 @@ while running:
         keys = pygame.key.get_pressed()
         player.movement(keys)
         player.draw(screen)
-        spawn_timer = 2
-        spawn_timer -= dt
-        if spawn_timer <= 0:
-            spawn_enemy(randint(1, 5), "Bottle Right/Bottle.pg1.png")
-            spawn_timer = 2
-        for enemy in enemies:
-            dx = player.x - enemy.x
-            dy = player.y - enemy.y
+        # spawn_timer = 2
+        # spawn_timer -= dt
+        # if spawn_timer <= 0:
+        #     spawn_enemy(randint(1, 5), "Assets/BottleL1.png")
+        #     spawn_timer = 2
+        # for enemy in enemies:
+        #     # dx = player.x - enemy.x
+        #     # dy = player.y - enemy.y
 
-            if dx > 0:
-                enemy.x += enemy_speed
-            if dx < 0:
-                enemy.x -= enemy_speed
-            if dy > 0:
-                enemy.y += enemy_speed
-            if dy < 0:
-                enemy.y -= enemy_speed
-            player_rect = pygame.Rect(player.x, player.y, 50, 50)
-            enemy_rect = pygame.Rect(enemy.x, enemy.y, 50, 50)
-            if player_rect.colliderect(enemy_rect):
-                player.hp -= 1
-                enemy.hp -= 1
-                print(player.hp)
-                if player.hp < 1:
-                    state = "game over"
-                if enemy.hp < 1:
-                    enemies.remove(enemy)
-            enemy.draw(screen)
+        #     # if dx > 0:
+        #     #     enemy.x += enemy_speed
+        #     # if dx < 0:
+        #     #     enemy.x -= enemy_speed
+        #     # if dy > 0:
+        #     #     enemy.y += enemy_speed
+        #     # if dy < 0:
+        #     #     enemy.y -= enemy_speed
+        #     player_rect = pygame.Rect(player.x, player.y, 50, 50)
+        #     enemy_rect = pygame.Rect(enemy.x, enemy.y, 50, 50)
+        #     if player_rect.colliderect(enemy_rect):
+        #         player.hp -= 1
+        #         enemy.hp -= 1
+        #         print(player.hp)
+        #         if player.hp < 1:
+        #             state = "game over"
+        #         if enemy.hp < 1:
+        #             enemies.remove(enemy)
+        #     enemy.draw(screen)
     elif state == "game over":
     # show game over screen
         screen.fill((80, 30, 30))
