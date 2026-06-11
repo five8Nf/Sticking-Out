05dffc93f6d62562ecb9b233eb3d418f2a003ac2
five8Nf <ashtonlimex@gmail.com>
2026-06-11 23:47:36 +0800

    finnish

--- Entities.py
+++ Entities.py
@@ -34,7 +34,8 @@ class Player:
         self.hp = hp
         self.frame = 1
         self.speed_y = 5
-        self.iframes = 0
+        self.iframes = 120
+
         self.ground_collision = False
         self.img = pygame.image.load(img).convert_alpha()
         self.img = pygame.transform.scale(self.img, (100, 100))
@@ -42,27 +43,28 @@ class Player:
     def draw(self, screen):
         screen.blit(self.img, (self.x, self.y))
     
-    def movement(self, keys):
-        if not (keys[pygame.K_a] and keys[pygame.K_d]):
-            if keys[pygame.K_a]:
-                self.img = pygame.image.load(f"Assets/PlayerL{floor(self.frame)%4}.png").convert_alpha()
-                self.img = pygame.transform.scale(self.img, (100, 100))
-                self.frame += 0.5
-            if keys[pygame.K_d]:
+    def movement(self, keys, screen):
+            mousex = pygame.mouse.get_pos()[0]
+            if mousex > self.x:
                 self.img = pygame.image.load(f"Assets/PlayerR{floor(self.frame)%4}.png").convert_alpha()
                 self.img = pygame.transform.scale(self.img, (100, 100))
                 self.frame += 0.5
-
+            else:
+                self.img = pygame.image.load(f"Assets/PlayerL{floor(self.frame)%4}.png").convert_alpha()
+                self.img = pygame.transform.scale(self.img, (100, 100))
+                self.frame += 0.5
+                
 class Enemy:
     def __init__(self, img, hp):
-        self.x = randint(0, 640)
-        self.y = randint(0, 400)
+        self.x = randint(-200, 0)
+        self.y = 350
         self.speed_y = 0
         self.jump_height = 10
         self.ground_collision = False
         self.hp = hp
         self.frame = 1
         self.speed = 9
+        self.iframes = 0
         self.img = pygame.image.load(img).convert_alpha()
         self.img = pygame.transform.scale(self.img, (100, 100))
     
@@ -77,16 +79,33 @@ class Player_Bullet:
         self.speed = speed
         self.jump_height = 10
         self.lifespan = 3600
-        self.type = variation
         self.img = pygame.image.load("Assets/Ammo.png").convert_alpha()
         self.img = pygame.transform.scale(self.img, (100, 100))
     
     def draw(self, screen):
         screen.blit(self.img, (self.x, self.y))
     
-    def update():
+    def update(self):
         self.lifespan -= 1
         self.x += self.speed
+
+# class Enemy_Bullet:
+#     def __init__(self, speed, x, y, px, py):
+#         self.x = x
+#         self.y = y
+#         self.speed_y = 0
+#         self.speed = speed
+#         self.jump_height = 10
+#         self.lifespan = 3600
+#         self.img = pygame.image.load(f"Assets/Projectile{randint(0, 1)}.png").convert_alpha()
+#         self.img = pygame.transform.scale(self.img, (100, 100))
+    
+#     def draw(self, screen):
+#         screen.blit(self.img, (self.x, self.y))
+    
+#     def update(self):
+#         self.lifespan -= 1
+#         self.x += 
 
 def spawn_enemy(num, img):
     global enemy_num
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 from Entities import *
 
 pygame.init()
@@ -10,13 +11,14 @@ clock = pygame.time.Clock()
 
 x = 0
 hp = []
-for i in range(5):
+for i in range(15):
     hp.append(Health(x, 0))
     x += 50
-player = Player("Assets/PlayerL1.png", 5)
+player = Player("Assets/PlayerL1.png", 15)
 
 speed = 10
 gravity = 0.3
+enemy_name = "Bottle"
 tiles = []
 walls = {"nofall": [], "Bottom": [], "Left": [], "Right": []}
 
@@ -32,7 +34,7 @@ def level_rect(amt_x, amt_y, x, y):
         tiles.append(Tile("Assets/Tile_down.png", x, y))
         x -= 100
         
-    spawn_enemy(5, "Assets/BottleL1.png")
+    spawn_enemy(50, "Assets/BottleL1.png")
     
     for enemy in enemies:
         if platform_rect.left <= enemy.x <= platform_rect.right:
@@ -45,7 +47,7 @@ def update_physics():
         player.speed_y += gravity
 
     if player.speed_y != 0:
-        player_hitbox = pygame.Rect(player.x, player.y, 20, 100)
+        player_hitbox = pygame.Rect(player.x, player.y, 100, 100)
         for bottom in walls["Bottom"]:
             if player_hitbox.colliderect(bottom):
                 player.speed_y = 0
@@ -54,13 +56,15 @@ def update_physics():
         for wall_list in walls.values():
             for rect in wall_list: 
                 rect.y -= player.speed_y
-        for bullet in bullet_list:
+        for bullet in nice_bullet_list:
+            bullet.y -= player.speed_y
+        for bullet in enemy_bullet_list:
             bullet.y -= player.speed_y
         for enemy in enemies:
             if not enemy.ground_collision:
                 enemy.y -= player.speed_y
 
-    player_hitbox = pygame.Rect(player.x, player.y, 20, 100)
+    player_hitbox = pygame.Rect(player.x, player.y, 100, 100)
     player_sensor = pygame.Rect(player.x, player.y + 100, 100, 2)
     player.ground_collision = False
 
@@ -68,9 +72,15 @@ def update_physics():
         for base in walls["nofall"]:
             if player_sensor.colliderect(base) or player_hitbox.colliderect(base):
                 correction = player_hitbox.bottom - base.top
-                for tile in tiles: tile.y += correction
+                for tile in tiles:
+                    tile.y += correction
                 for wall_list in walls.values():
-                    for rect in wall_list: rect.y += correction
+                    for rect in wall_list: 
+                        rect.y += correction
+                for bullet in nice_bullet_list:
+                    bullet.y += correction
+                for bullet in enemy_bullet_list:
+                    bullet.y += correction
                 for enemy in enemies:
                     if not enemy.ground_collision:
                         enemy.y += correction
@@ -100,9 +110,9 @@ def update_physics():
             enemy.speed_y += gravity
             enemy.y += enemy.speed_y
 
-def movement(keys):
-    player.movement(keys)
-    
+def movement(keys, screen):
+    player.movement(keys, screen)
+
     if keys[pygame.K_w] and player.ground_collision:
         player.speed_y = -12
         player.ground_collision = False
@@ -122,62 +132,115 @@ def movement(keys):
             for rect in wall_list: rect.x -= speed
 
 def enemy_move():
-    global state
-    player_hitbox = pygame.Rect(player.x, player.y, 30, 100)
+    global state, enemy_name
+    player_hitbox = pygame.Rect(player.x, player.y, 100, 100)
     for enemy in enemies:
-        enemy_hitbox = pygame.Rect(enemy.x, enemy.y, 30, 100)
-        if enemy_hitbox.colliderect(player_hitbox) and player.iframes == 0:
+        enemy_hitbox = pygame.Rect(enemy.x, enemy.y, 100, 100)
+        if enemy_hitbox.colliderect(player_hitbox) and player.iframes < 1:
             player.hp -= 1
             if player.hp < 1:
                 state = "died"
             else:
                 hp.pop(player.hp)
-                player.iframes = 90
+                player.iframes = 120
         enemy.frame += 1
         if enemy.x > player.x:
-            enemy.x -= 9
-            enemy.img = pygame.image.load(f"Assets/BottleR{enemy.frame%4}.png").convert_alpha()
+            enemy.x -= randint(8, 10)
+            enemy.img = pygame.image.load(f"Assets/{enemy_name}R{enemy.frame%4}.png").convert_alpha()
             enemy.img = pygame.transform.scale(enemy.img, (100, 100))
         else:
-            enemy.x += 9
-            enemy.img = pygame.image.load(f"Assets/BottleL{enemy.frame%4}.png").convert_alpha()
+            enemy.x += randint(8, 10)
+            enemy.img = pygame.image.load(f"Assets/{enemy_name}L{enemy.frame%4}.png").convert_alpha()
             enemy.img = pygame.transform.scale(enemy.img, (100, 100))
 
 running = True
-state = "playing"
+state = "menu"
 pygame.mixer.music.load("Assets/bg_sound.mp3")
 pygame.mixer.music.play(-1)
-level_rect(25, 1, 320, 450)
-bullet_list = []
+level_rect(50, 1, 320, 450)
+nice_bullet_list = []
+enemy_bullet_list = []
 
 while running:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
-    if state == "playing":
-        screen.fill((0, 0, 0))
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mousex = pygame.mouse.get_pos()[0]
+            if mousex > player.x:
+                nice_bullet_list.append(Player_Bullet(10, player.x, player.y+25))
+            else:
+                nice_bullet_list.append(Player_Bullet(-10, player.x, player.y+25))
+    if state == "menu":
+        img = pygame.image.load("Assets/Coverpage.png").convert_alpha()
+        img = pygame.transform.scale(img, (1280, 720))
+        screen.blit(img, (0, 0))
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            state = "playing"
+    elif state == "playing":
+        img = pygame.image.load("Assets/Wood background.png").convert_alpha()
+        img = pygame.transform.scale(img, (1280, 720))
+        screen.blit(img, (0, 0))
         
         keys = pygame.key.get_pressed()
-        movement(keys)
+        movement(keys, screen)
         update_physics()
         enemy_move()
         
         for heart in hp:
             heart.draw(screen)
         player.draw(screen)
+        for index in range(len(nice_bullet_list)):
+            bullet_hitbox = pygame.Rect(nice_bullet_list[index].x, nice_bullet_list[index].y, 100, 100)
+            for enemy in enemies:
+                enemy_hitbox = pygame.Rect(enemy.x, enemy.y, 100, 100)
+                if bullet_hitbox.colliderect(enemy_hitbox) and enemy.iframes < 1:
+                    if randint(1, 3) == 1:
+                        enemy.hp -= 1
+                        enemy.iframes = 60
+        enemies = [enemy for enemy in enemies if enemy.hp > 0]
+        to_pop = []
+        for index in range(len(enemy_bullet_list))  :
+            bullet_hitbox = pygame.Rect(enemy_bullet_list[index].x, enemy_bullet_list[index].y, 100, 100)
+            player_hitbox = pygame.Rect(enemy.x, enemy.y, 100, 100)
+            if bullet_hitbox.colliderect(player_hitbox) and player.iframes < 1:
+                player.hp -= 1
+                if player.hp < 1:
+                    state = "died"
+                else:
+                    hp.pop(player.hp)
+                    player.iframes = 120
+                to_pop.append(i)
+        for index in to_pop:
+            enemy_bullet_list.pop(index)
+
+        for bullet in nice_bullet_list:
+            bullet.update()
+            bullet.draw(screen)
+
         for enemy in enemies:
-            enemy.draw(screen)
+            enemy.iframes -= 1
+            if enemy.hp > 0:
+                enemy.draw(screen)
         for tile in tiles:
             tile.draw(screen)
-        player.iframes -= 1
+        
+        # if enemy_name == "Boss ":
+        #     if randint(1, 5) == "5":
+        #         enemy_bullet_list.append(Enemy_Bullet(20, enemies[0].x, enemies[0].y, player.x, player.y))
 
+        if len(enemies) == 0:
+            enemies.append(Enemy("Assets/Boss L1.png", 50))
+            enemy_name = "Boss "
+
+        player.iframes -= 1
     elif state == "died":
         img = pygame.image.load("Assets/Death screen.png").convert_alpha()
         img = pygame.transform.scale(img, (1280, 720))
         screen.blit(img, (0, 0))
-    
+
     pygame.display.update()
 
 pygame.quit()
